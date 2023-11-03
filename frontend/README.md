9e8bb3498f17b4485c4eb916124928aea2c5c786
linzui <yangminghe20@gmail.com>
2023-11-03 19:09:11 +0800

    前端代码构建

---
+++ frontend/README.md
@@ -0,0 +1,13 @@
+# frontend
+
+## 项目设置
+
+```sh
+npm install
+```
+
+### 编译和热重载以进行开发
+
+```sh
+npm run dev
+```
---
+++ frontend/src/views/web/home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home-view">
+
+    <div class="home-nav">
+      <div class="home-banner">
+        <div class="left">
+          <span>HYM</span>
+        </div>
+        <div class="center">
+          <span>首页</span>
+          <span>关于</span>
+        </div>
+        <div class="right">
+          <span>搜索</span>
+        </div>
+      </div>
+    </div>
+    
+    
+
+    <div class="home-container">
+
+    </div>
+
+    <div class="home-footer">
+
+    </div>
+
+
+  </div>
+</template>
+
+
+<script setup lang="ts">
+ 
+</script>
+
+
+<style scoped>
+.home-nav {
+  background-color: white;
+  height: 60px;
+  box-shadow: 1px 1px 5px #0003;
+  display: flex;
+  justify-content: center;
+}
+
+.home-container {
+  background-color: #f0eeee;
+  width: 1200px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
