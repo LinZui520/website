fa99adf11dc58bc2c9b05d34472f736a2db17cb5
linzui <yangminghe20@gmail.com>
2023-12-22 01:06:57 +0800

    mysql

--- database/mysql/init.sql
+++ database/mysql/init.sql
@@ -1,42 +1,30 @@
+drop database website;
 create database if not exists website;
 use website;
-create table if not exists users (
-    id int unsigned auto_increment comment '用户编号',
-    nickname varchar(32) not null comment '昵称',
+
+drop table if exists users;
+create table users (
+    id int auto_increment comment '用户编号',
+    avatar varchar(32) comment '头像编号',
     username varchar(32) not null comment '用户名',
+    email varchar(32) not null comment '邮箱',
     password varchar(64) not null comment '密码',
-    power int default 0 not null comment '用户权限',
-    creation datetime not null comment '创建时间',
-    latest datetime null comment '最近登陆时间',
+    power int default 0 comment '用户权限',
+    register datetime comment '创建时间',
+    login datetime comment '最近登陆时间',
     primary key (id),
-    unique key (username)
+    unique key (username),
+    unique key (email)
 ) AUTO_INCREMENT=100000;
 
-create table if not exists images (
-    id int unsigned auto_increment comment '图片编号',
-    filename  varchar(64) not null comment '图片文件名',
-    creation datetime not null comment '创建时间',
-    primary key (id),
-    unique key (filename)
-) AUTO_INCREMENT=100000;
 
 create table if not exists articles (
-    id int unsigned auto_increment comment '文章编号',
-    author int unsigned not null comment '作者编号',
-    image int unsigned not null comment '标题图片编号',
+    id int auto_increment comment '文章编号',
+    author int not null comment '作者编号',
     title varchar(256) not null comment '标题',
     content longtext null comment '内容',
-    creation datetime not null comment '创建时间',
-    latest datetime not null comment '最近更新时间',
+    `create` datetime not null comment '创建时间',
+    `update` datetime not null comment '最近更新时间',
     primary key (id),
-    foreign key (author) references users(id),
-    foreign key (image) references images (id)
-) AUTO_INCREMENT=100000;
-
-create table if not exists messages (
-    id int unsigned auto_increment comment '留言编号',
-    author int unsigned not null comment '留言人编号',
-    content longtext null comment '留言内容',
-    creation datetime not null comment '创建时间',
-    primary key (id)
+    foreign key (author) references users(id)
 ) AUTO_INCREMENT=100000;
