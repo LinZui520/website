acbbf6ba870920255b4fab67893f25a686e06dbc
linzui <yangminghe20@gmail.com>
2024-01-07 14:08:59 +0800

    主页

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,32 +1,39 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Footer from '../components/index/Footer';
 import {motion} from "framer-motion";
+import Header from "../components/memories/21/Header";
+import First from "../components/memories/21/First";
+import Second from "../components/memories/21/Second";
+import Third from "../components/memories/21/Third";
+import useHandleWheel from "../hooks/useHandleWheel";
 
 const Home = () => {
 
+  const {handleWheel} = useHandleWheel()
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   
   return (
-    <div>
+    <div  onWheel={handleWheel}>
       <div className={"flex flex-col justify-center items-center h-screen"}>
         <motion.div
           whileHover={{scale: 1.2}}
           whileTap={{scale: 0.9}}
           drag
-          dragConstraints={{
-            top: -100,
-            left: -100,
-            right: 100,
-            bottom: 100,
-          }}
+          dragConstraints={{top: 0, left: 0, right: 0, bottom: 0}}
           className={"text-[#1d1d1f] text-[64px]"}
         >
           朱贵是混蛋
         </motion.div>
       </div>
+
+      <Header/>
+      <First/>
+      <Second/>
+      <Third/>
+
       <Footer/>
     </div>
   );
