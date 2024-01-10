b36991d4de591449bafaf9560c2a9a48e3964a2a
linzui <yangminghe20@gmail.com>
2024-01-10 16:42:52 +0800

    memories Home

--- web/src/pages/memories/HomeMemories.tsx
+++ web/src/pages/memories/HomeMemories.tsx
@@ -10,9 +10,11 @@ const HomeMemories = () => {
       whileTap={{scale: 0.9}}
       drag
       dragConstraints={{top: -0, left: -0, right: 0, bottom: 0}}
-      className={"flex flex-col justify-center items-center h-screen text-[#1d1d1f] text-[54px]"}
+      className={"flex flex-col justify-center items-center h-screen text-[#1d1d1f] text-[16px] lg:text-[32px]"}
     >
-      Hello Memories
+      如果你想添加属于你自己班级的独家回忆录
+      <br />
+      欢迎联系我 yangminghe20@gmail.com
     </motion.div>
   );
 }
