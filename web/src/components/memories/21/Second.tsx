b4e0702721f4642e3b571faca747ad7ef8e1c0c5
linzui <yangminghe20@gmail.com>
2024-02-13 18:20:44 +0800

    update: better

--- web/src/components/memories/21/Second.tsx
+++ web/src/components/memories/21/Second.tsx
@@ -19,11 +19,10 @@ const Second = React.memo(() => {
 
   return (
     <div ref={ref} className={"h-[200vh] w-screen flex flex-col items-center overflow-clip"}>
-      <div className={"h-[50vh] w-screen"}/>
       <div
         className={
-          "h-[50vh] w-[90vw] max-w-[1024px] text-[16px] lg:text-[32px] " +
-          "sticky top-[50vh] select-none flex flex-row justify-evenly"
+          "h-[100vh] w-[90vw] max-w-[1024px] text-[16px] lg:text-[32px] " +
+          "sticky top-0 select-none flex flex-row justify-evenly items-center"
         }
       >
         <div>
@@ -45,7 +44,6 @@ const Second = React.memo(() => {
             近到可以听到他的心跳
           </motion.div>
         </div>
-
       </div>
 
       <div className={"h-screen w-screen flex justify-center items-center sticky top-0 bottom-0"}>
