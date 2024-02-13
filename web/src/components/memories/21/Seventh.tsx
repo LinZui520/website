8917be4ceee3165c82edc0654d3c7da19e477c3e
Lin_Zui <fenggaoliu20@gmail.com>
2024-02-13 15:55:07 +0800

    feat: Seventh

---
+++ web/src/components/memories/21/Seventh.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Seventh = React.memo(() => {
+  return (
+    <div className="w-screen h-[1000vh]" >
+      <div className="w-screen h-screen flex flex-col justify-center items-center" >
+        我自横刀向天笑
+      </div>
+      <div className="w-screen h-screen" >2</div>
+      <div className="w-screen h-screen" >3</div>
+      <div className="w-screen h-screen" >4</div>
+      <div className="w-screen h-screen" >5</div>
+      <div className="w-screen h-screen" >6</div>
+      <div className="w-screen h-screen" >7</div>
+      <div className="w-screen h-screen" >8</div>
+      <div className="w-screen h-screen" >9</div>
+      <div className="w-screen h-screen" >10</div>
+    
+    </div>
+  )
+})
+
+export default Seventh
