24313227679f04aa39cecbc9f3eccd9ea840190e
Lin_Zui <fenggaoliu20@gmail.com>
2024-02-15 16:53:28 +0800

    save: Seventh

--- web/src/components/memories/21/Seventh.tsx
+++ web/src/components/memories/21/Seventh.tsx
@@ -1,4 +1,13 @@
 import React from "react"
+import FirstImage from "../../../assets/image/memories/21/2024-02-15-16-12-55.png"
+import SecondImage from "../../../assets/image/memories/21/2024-02-15-16-13-55.png"
+import ThirdImage from "../../../assets/image/memories/21/2024-02-15-16-14-55.png"
+import FourthImage from "../../../assets/image/memories/21/2024-02-15-16-15-55.png"
+import FifthImage from "../../../assets/image/memories/21/2024-02-15-16-16-55.png"
+import SixthImage from "../../../assets/image/memories/21/2024-02-15-16-17-55.png"
+import SeventhImage from "../../../assets/image/memories/21/2024-02-15-18-12-55.png"
+import EighthImage from "../../../assets/image/memories/21/2024-02-15-16-19-55.png"
+import NinthImage from "../../../assets/image/memories/21/2024-02-15-16-20-55.png"
 
 const Seventh = () => {
   return (
