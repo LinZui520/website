48eaab4d62a5d6b9512493c465df066d41939df9
linzui <yangminghe20@gmail.com>
2024-02-16 13:51:59 +0800

    feat: eighth

---
+++ web/src/components/memories/21/Eighth.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import FirstImage from "../../../assets/image/memories/21/2024-02-16-13-14-38.png";
+import SecondImage from "../../../assets/image/memories/21/2024-02-16-13-15-38.png"
+import ThirdImage from "../../../assets/image/memories/21/2024-02-16-13-16-38.png"
+import FourthImage from "../../../assets/image/memories/21/2024-02-16-13-17-38.png"
+import FifthImage from "../../../assets/image/memories/21/2024-02-16-13-18-38.png"
+import SixthImage from "../../../assets/image/memories/21/2024-02-16-13-19-38.png"
+import SeventhImage from "../../../assets/image/memories/21/2024-02-16-13-20-38.png"
+import EighthImage from "../../../assets/image/memories/21/2024-02-16-13-21-38.png"
+import NinthImage from "../../../assets/image/memories/21/2024-02-16-13-22-38.png"
+import TenthImage from "../../../assets/image/memories/21/2024-02-16-13-23-38.png"
+
+
+const Eighth = () => {
+
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default React.memo(Eighth);
