19263f885e26f657a841bb28297c31cffb69f372
Lin_Zui <fenggaoliu20@gmail.com>
2024-02-16 16:41:36 +0800

    updata: Seventh

--- web/src/components/memories/21/Seventh.tsx
+++ web/src/components/memories/21/Seventh.tsx
@@ -28,20 +28,52 @@ const Seventh = () => {
 
   return (
     <div ref={ref} className={"w-screen h-[1000vh]"} >
-      <div className={"h-screen w-screen flex flex-col justify-center items-center sticky top-0 bottom-0 overflow-hidden" }>
+      <div className={"h-screen w-screen flex flex-row justify-evenly items-center sticky top-0 bottom-0 overflow-hidden"}>
         <motion.img
-          className="h-[60vw] w-[60vw] max-h-[300px] max-w-[300px] rounded-[16px] overflow-hidden"
+          className={"h-[30vw] w-[30vw] max-h-[600px] max-w-[600px] rounded-[16px] overflow-hidden"}
           src={FirstImage} alt={""}
           animate={{
             opacity: 9 * (1 / 9 - value),
             scale: 9 * (1 / 9 - value),
-            x: -9 * value * windowWidth + 'px'
+            y: windowHeight * 9 * value + 'px'
           }}
           transition={{ease: "easeOut", duration: 0.618}}
         />
+
+        <motion.span 
+          animate={{
+            opacity: 9 * (1 / 9 - value),
+            scale: 9 * (1 / 9 - value),
+            y: -9 * value * windowHeight + 'px'
+          }}
+          transition={{ease: "easeOut", duration: 0.618}}
+          className={"text-[16px] lg:text-[32px] max-w-[30vw] select-none overflow-hidden"}>
+        有时候，挺无奈的，挺惆怅的……这图片是不是可以代表我们的心情？
+        </motion.span>
       </div>
-      
-      <div className="w-screen h-screen" >2</div>
+
+      <div className="h-screen w-screen flex flex-row justify-evenly items-center sticky top-0 bottom-0 overflow-hidden">
+        <motion.img 
+          className={"h-[30vw] w-[30vw] max-h-[309px] max-w-[500px] rounded-[16px] overflow-hidden"}
+          src={SecondImage} alt={""}
+          animate={{
+            opacity: 9 * value,
+            y: (9 * value - 1) * windowHeight + 'px'
+          }}
+          transition={{ease: "easeOut", duration: 0.618}}
+        />
+
+        <motion.span 
+          animate={{
+              opacity: 9 * value,
+             y: -(9 * value - 1) * windowHeight + 'px'
+            }}
+            transition={{ease: "easeOut", duration: 0.618}}
+            className={"text-[16px] lg:text-[32px] max-w-[30vw] select-none overflow-hidden"}>
+        贺阳明同学说：“错的不是我，而是整个世界。”怪默写喽！注：此图不是他的杰作
+        </motion.span>
+      </div>
+
       <div className="w-screen h-screen" >3</div>
       <div className="w-screen h-screen" >4</div>
       <div className="w-screen h-screen" >5</div>
