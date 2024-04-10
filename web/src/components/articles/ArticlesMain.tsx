bff89ed91dee4540501f472af8107bde53a8c616
linzui <yangminghe20@gmail.com>
2024-04-11 00:11:10 +0800

    style: 博客界面

--- web/src/components/articles/ArticlesMain.tsx
+++ web/src/components/articles/ArticlesMain.tsx
@@ -1,7 +1,7 @@
-import {motion} from "framer-motion";
 import {useNavigate} from "react-router-dom";
 import useFetchArticles from "../../hooks/article/useFetchArticles";
 import React from "react";
+import { motion } from "framer-motion";
 
 const ArticlesMain = () => {
 
@@ -9,44 +9,61 @@ const ArticlesMain = () => {
   const {articles} = useFetchArticles()
 
   return (
-    <div className={"w-screen bg-[#1d1d1f] min-h-screen flex flex-col justify-evenly items-center"}>
+    <div className={"w-screen bg-[#fbfbfd] min-h-screen flex flex-col justify-evenly items-center"}>
       {articles.map(item =>
-        <motion.div
+        <div
           key={item.id}
-          onClick={() => navigate('/article/' + item.id)}
-          whileHover={{scale: 1.1}}
-          whileTap={{scale: 0.9}}
-          initial={{opacity: 0, scale: 0}}
-          whileInView={{opacity: 1, scale: 1}}
-          transition={{duration: 0.5}}
           className={
-            "border-2 border-white w-[90vw] h-[45vw] m-[5vw] " +
-            "max-w-[600px] max-h-[300px] flex flex-col " +
-            "cursor-pointer select-none rounded-[25px]"
+            "h-[86px] w-[90vw] md:w-[80vw] lg:w-[60vw] max-w-[768px] bg-[#fbfbfd] mb-[48px] lg:mb-[20px] " +
+            "flex flex-col justify-between items-center select-none"
           }
         >
-          <div className={"flex flex-row items-center w-[100%] mt-[16px]"}>
-            <img
-              src={`${window.location.origin}/image/${item.avatar}`} alt={""}
-              className={"w-[32px] h-[32px] lg:w-[64px] lg:h-[64px] " +
-                "object-contain rounded-full select-none ml-[16px] mr-[16px]"}
-            />
-            <div className={"flex flex-col justify-center"}>
-              <div className={"text-[12px] lg:text-[24px] text-[#fbfbfd]"}>
-                {item.username}
-              </div>
-              <div className={"text-[8px] lg:text-[16px] text-[#fbfbfd]"}>
-                {new Date(item.create).toLocaleString()}
-              </div>
+          <div className={"w-[100%] flex flex-row justify-between items-center"}>
+            <div className={"flex flex-row justify-center items-center"}>
+              <motion.span
+                initial={{opacity: 0, scale: 0}}
+                whileInView={{opacity: 1, scale: 1}}
+                transition={{duration: 0.25}}
+                className={"italic text-[16px] lg:text-[24px] text-[#1d1d1f] mr-[32px]"}
+              >
+                {`${new Date(item.create).getFullYear()}/${(new Date(item.create).getMonth() + 1).toString().padStart(2, '0')}/${new Date(item.create).getDate().toString().padStart(2, '0')}`}
+              </motion.span>
+
+              <motion.h1
+                onClick={() => navigate('/article/' + item.id)}
+                whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}
+                initial={{opacity: 0, scale: 0}}
+                whileInView={{opacity: 1, scale: 1}}
+                transition={{duration: 0.25}}
+                className={"font-bold text-[24px] lg:text-[32px] text-[#1d1d1f] cursor-pointer"}
+              >
+                {item.title}
+              </motion.h1>
+            </div>
+
+            <div className={"flex flex-row justify-center items-center"}>
+              <motion.img
+                initial={{opacity: 0, scale: 0}}
+                whileInView={{opacity: 1, scale: 1}}
+                transition={{duration: 0.25}}
+                src={`${window.location.origin}/image/${item.avatar}`}
+                alt={""} title={item.username}
+                className={
+                  "w-[32px] h-[32px] lg:w-[64px] lg:h-[64px] " +
+                  "object-contain rounded-full select-none ml-[16px] mr-[16px]"
+                }
+              />
             </div>
           </div>
 
-          <div className={"flex flex-auto flex-col justify-center items-center w-[100%]"}>
-            <h1 className={"text-[16px] lg:text-[24px] text-[#fbfbfd]"}>
-              {item.title}
-            </h1>
-          </div>
-        </motion.div>
+          <motion.hr
+            initial={{opacity: 0, scale: 0}}
+            whileInView={{opacity: 1, scale: 1}}
+            transition={{duration: 0.25}}
+            className={"w-[100%] border-t border-dashed border-[#1d1d1f]"}
+          />
+        </div>
       )}
     </div>
   );
