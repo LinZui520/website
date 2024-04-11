59ee0980cd22774bcf5220a0e799dce150911172
linzui <yangminghe20@gmail.com>
2024-04-11 14:25:38 +0800

    style: 太美丽了

--- web/src/components/articles/ArticlesMain.tsx
+++ web/src/components/articles/ArticlesMain.tsx
@@ -1,6 +1,6 @@
 import {useNavigate} from "react-router-dom";
 import useFetchArticles from "../../hooks/article/useFetchArticles";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 const ArticlesMain = () => {
@@ -8,23 +8,38 @@ const ArticlesMain = () => {
   const navigate = useNavigate()
   const {articles} = useFetchArticles()
 
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1024);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1024);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
   return (
-    <div className={"w-screen bg-[#fbfbfd] min-h-screen flex flex-col justify-evenly items-center"}>
+    <div className={"w-screen bg-[#fbfbfd] min-h-screen flex flex-col justify-evenly items-center overflow-clip"}>
       {articles.map(item =>
         <div
           key={item.id}
           className={
-            "h-[86px] w-[90vw] md:w-[80vw] lg:w-[60vw] max-w-[768px] bg-[#fbfbfd] mb-[48px] lg:mb-[20px] " +
+            "h-[86px] w-[90vw] md:w-[80vw] lg:w-[60vw] max-w-[768px] bg-[#fbfbfd] mb-[56px] md:mb-[48px] lg:mb-[20px] " +
             "flex flex-col justify-between items-center select-none"
           }
         >
           <div className={"w-[100%] flex flex-row justify-between items-center"}>
             <div className={"flex flex-row justify-center items-center"}>
               <motion.span
-                initial={{opacity: 0, scale: 0}}
-                whileInView={{opacity: 1, scale: 1}}
-                transition={{duration: 0.25}}
-                className={"italic text-[16px] lg:text-[24px] text-[#1d1d1f] mr-[32px]"}
+                initial={{x: -64, opacity: 0}}
+                whileInView={{x: 0, opacity: 1}}
+                transition={{duration: 0.5}}
+                className={"italic text-[12px] md:text-[16px] lg:text-[24px] text-[#1d1d1f] mr-[32px]"}
               >
                 {`${new Date(item.create).getFullYear()}/${(new Date(item.create).getMonth() + 1).toString().padStart(2, '0')}/${new Date(item.create).getDate().toString().padStart(2, '0')}`}
               </motion.span>
@@ -35,18 +50,18 @@ const ArticlesMain = () => {
                 whileTap={{scale: 0.9}}
                 initial={{opacity: 0, scale: 0}}
                 whileInView={{opacity: 1, scale: 1}}
-                transition={{duration: 0.25}}
-                className={"font-bold text-[24px] lg:text-[32px] text-[#1d1d1f] cursor-pointer"}
+                transition={{duration: 0.5}}
+                className={"font-bold text-[18px] md:text-[24px] lg:text-[32px] text-[#1d1d1f] cursor-pointer"}
               >
                 {item.title}
               </motion.h1>
             </div>
 
-            <div className={"flex flex-row justify-center items-center"}>
+            <div className={"flex flex-row justify-end items-center"}>
               <motion.img
-                initial={{opacity: 0, scale: 0}}
-                whileInView={{opacity: 1, scale: 1}}
-                transition={{duration: 0.25}}
+                initial={isLargeScreen ? {x: 100, rotate: 180, opacity: 0} : {x: 50, rotate: 180, opacity: 0}}
+                whileInView={{x: 0, rotate: 0, opacity: 1}}
+                transition={{duration: 0.5}}
                 src={`${window.location.origin}/image/${item.avatar}`}
                 alt={""} title={item.username}
                 className={
@@ -58,9 +73,9 @@ const ArticlesMain = () => {
           </div>
 
           <motion.hr
-            initial={{opacity: 0, scale: 0}}
-            whileInView={{opacity: 1, scale: 1}}
-            transition={{duration: 0.25}}
+            initial={{scale: 0}}
+            whileInView={{scale: 1}}
+            transition={{duration: 0.5}}
             className={"w-[100%] border-t border-dashed border-[#1d1d1f]"}
           />
         </div>
