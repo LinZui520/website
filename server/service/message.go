70783f09fdcae0a572a3c04c99a11e5355a14071
linzui <yangminghe20@gmail.com>
2024-04-13 13:54:58 +0800

    feat: 留言缓存功能

--- server/service/message.go
+++ server/service/message.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/gin-gonic/gin"
 	"server/global"
@@ -9,6 +10,8 @@ import (
 )
 
 type MessageService struct{}
+
+const messageCacheKey = "messages"
 
 func (MessageService) AddMessage(c *gin.Context) error {
 	tokenString, _ := c.Cookie("token")
@@ -22,12 +25,33 @@ func (MessageService) AddMessage(c *gin.Context) error {
 		Content: c.PostForm("content"),
 		Create:  time.Now(),
 	}
-	return global.DB.Create(&message).Error
+	if global.DB.Create(&message).Error != nil {
+		return errors.New("添加失败")
+	}
+
+	err = global.Redis.Del(messageCacheKey).Err()
+	if err != nil {
+		global.Log.Warnln("Redis 删除留言缓存失败:", err)
+	}
+
+	return nil
 }
 
 func (MessageService) GetAllMessage() ([]model.MessageDTO, error) {
 	var messages []model.MessageDTO
-	err := global.DB.Table("messages").
+
+	cachedMessages, err := global.Redis.Get(messageCacheKey).Result()
+	if err == nil {
+		err = json.Unmarshal([]byte(cachedMessages), &messages)
+		if err == nil {
+			global.Log.Infoln("Redis 缓存留言数据获取成功")
+			return messages, nil
+		} else {
+			global.Log.Warnln("Redis 缓存留言数据解析失败:", err)
+		}
+	}
+
+	err = global.DB.Table("messages").
 		Select("messages.id as Id, author, avatar, username, content, `create`").
 		Joins("LEFT JOIN users ON messages.author = users.id").
 		Order("`create` desc").
@@ -35,6 +59,16 @@ func (MessageService) GetAllMessage() ([]model.MessageDTO, error) {
 	if err != nil {
 		return nil, errors.New("查询失败")
 	}
+
+	serializedMessages, err := json.Marshal(messages)
+	if err != nil {
+		global.Log.Warnln("Redis 留言数据序列化失败:", err)
+	}
+	err = global.Redis.Set(messageCacheKey, serializedMessages, 10*time.Minute).Err()
+	if err != nil {
+		global.Log.Warnln("Redis 存储留言失败:", err)
+	}
+
 	return messages, nil
 }
 
@@ -55,7 +89,16 @@ func (MessageService) DeleteMessage(c *gin.Context) error {
 		return errors.New("没有权限删除该留言")
 	}
 
-	return global.DB.Where("id = ?", message.Id).Delete(&message).Error
+	if global.DB.Where("id = ?", message.Id).Delete(&message).Error != nil {
+		return errors.New("删除失败")
+	}
+
+	err = global.Redis.Del(messageCacheKey).Err()
+	if err != nil {
+		global.Log.Warnln("Redis 删除留言缓存失败:", err)
+	}
+
+	return nil
 }
 
 func (MessageService) GetMessageByAuthor(c *gin.Context) ([]model.MessageDTO, error) {
