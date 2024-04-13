de1739dc7687ce19add415dc4068a6586bb2ca65
linzui <yangminghe20@gmail.com>
2024-04-13 13:40:33 +0800

    style: log

--- server/core/logrus.go
+++ server/core/logrus.go
@@ -44,12 +44,12 @@ func (t *LogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	if entry.HasCaller() {
 		funcVal := entry.Caller.Function
 		fileVal := fmt.Sprintf("%s:%d", path.Base(entry.Caller.File), entry.Caller.Line)
-		_, err := fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s %s %s\n", log.Prefix, timestamp, levelColor, entry.Level, fileVal, funcVal, entry.Message)
+		_, err := fmt.Fprintf(b, "%s %s \x1b[%dm[%s]\x1b[0m %s %s %s\n", log.Prefix, timestamp, levelColor, entry.Level, fileVal, funcVal, entry.Message)
 		if err != nil {
 			return nil, err
 		}
 	} else {
-		_, err := fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s\n", log.Prefix, timestamp, levelColor, entry.Level, entry.Message)
+		_, err := fmt.Fprintf(b, "%s %s \x1b[%dm[%s]\x1b[0m %s\n", log.Prefix, timestamp, levelColor, entry.Level, entry.Message)
 		if err != nil {
 			return nil, err
 		}
