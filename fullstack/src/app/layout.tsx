f0411f1659dc02f478bb6b7c8844b6315dcabdcf
Yangming He <yangminghe20@gmail.com>
2024-05-03 23:55:47 +0800

    feat: 初始化next.js项目

---
+++ fullstack/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import React from "react";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "我不吃牛肉",
+  description: "一个简约风格的网站",
+};
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="zh">
+      <body>{children}</body>
+    </html>
+  );
+}
+
+export default RootLayout;
---
+++ fullstack/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1>我不吃牛肉</h1>
+      <p>一个简约风格的网站</p>
+    </div>
+  );
+}
+
+export default Home;
