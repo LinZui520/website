5b61d6d32942a24aef1c683e4a75a5cb9a478362
Yangming He <yangminghe20@gmail.com>
2024-05-03 22:28:06 +0800

    refactor: 清空仓库 准备重构

--- server/config/logger.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type Logger struct {
-	Level        string `yaml:"level"`
-	Prefix       string `yaml:"prefix"`
-	Director     string `yaml:"director"`
-	ShowLine     bool   `yaml:"show_line" mapstructure:"show_line"`
-	LogInConsole bool   `yaml:"log_in_console" mapstructure:"log_in_console"`
-}
--- server/config/redis.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type Redis struct {
-	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
-	Password string `yaml:"password"`
-	DB       int    `yaml:"db"`
-}
