f92e964ecf922c2b54cd1d3512de19d77165f7d6
Yangming He <yangminghe20@gmail.com>
2024-05-04 16:49:10 +0800

    feat: 完善article api

--- fullstack/src/app/api/article/[id]/route.ts
+++ fullstack/src/app/api/article/[id]/route.ts
@@ -9,15 +9,40 @@ export const GET = async (_request: NextRequest, { params }: { params: { id: str
         id: parseInt(params.id)
       },
       include: {
-        user: true
+        User: {
+          select: {
+            id: true, avatar: true, username: true, email: true,
+            password: false, power: true, register: false, login: false,
+          }
+        }
+      }
+    })
+    if (!article) return NextResponse.json(ResponseError('文章不存在'))
+    return NextResponse.json(ResponseOK(article))
+  } catch (error) {
+    return NextResponse.json(ResponseError('网络错误'))
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export const PUT = async (request: NextRequest, { params }: { params: { id: string } }) => {
+  try {
+    const formData = await request.formData()
+    const article = await prisma.article.update({
+      where: {
+        id: parseInt(params.id)
+      },
+      data: {
+        title: String(formData.get('title')) ,
+        content: String(formData.get('content')),
+        update: new Date()
       }
     })
     return NextResponse.json(ResponseOK(article))
   } catch (error) {
-
+    return NextResponse.json(ResponseError('文章不存在'))
   } finally {
     await prisma.$disconnect();
   }
-
-  return NextResponse.json(ResponseError('文章不存在'))
 }
--- fullstack/src/type/response.ts
+++ fullstack/src/type/response.ts
@@ -1,12 +1,20 @@
-import {Article} from "@prisma/client";
+import {Article, Conversation, Image, Message, User} from "@prisma/client";
+
+type data = null |
+  User | User[] |
+  Article | Article[] |
+  Comment | Comment[] |
+  Image | Image[] |
+  Message | Message[] |
+  Conversation | Conversation[]
 
 export interface Response {
   code: number;
   message: string;
-  data: Article | Article[] | null;
+  data: data;
 }
 
-export const ResponseOK = (data: Article | Article[] | null): Response => ({
+export const ResponseOK = (data: data): Response => ({
   code: 200,
   message: '成功',
   data
