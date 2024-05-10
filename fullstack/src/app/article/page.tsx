c7152038b17d688956ea39c427dc8362e7eb582c
Yangming He <yangminghe20@gmail.com>
2024-05-10 17:45:29 +0800

    fix: build bug

--- fullstack/src/app/article/page.tsx
+++ fullstack/src/app/article/page.tsx
@@ -1,21 +1,19 @@
-import { ArticlePrisma } from "@/types/article";
-import request from "@/lib/axios";
+// import { ArticlePrisma } from "@/types/article";
+// import request from "@/lib/axios";
 
 const Page = async () => {
 
-  const res = await request({
-    url: 'http://127.0.0.1:3000/api/article',
-    method: 'GET',
-  })
-
-  const articles: ArticlePrisma[] = res.data.data
+  // const res = await request({
+  //   url: 'http://127.0.0.1:3000/api/article',
+  //   method: 'GET',
+  // })
+  //
+  // const articles: ArticlePrisma[] = res.data.data
 
   return (
-    <>
-      {articles?.map(article => (
-        <div key={article.id} className={"h-[30vh]"}>{article.title} {article.User.username}</div>
-      ))}
-    </>
+    <div>
+      articles
+    </div>
   );
 }
 
