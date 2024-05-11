adf1b933d340e197226fb40247191eb012febf62
Yangming He <yangminghe20@gmail.com>
2024-05-11 10:45:25 +0800

    style: better

--- fullstack/src/app/api/auth/verify/route.ts
+++ fullstack/src/app/api/auth/verify/route.ts
@@ -6,14 +6,14 @@ import redis from "@/lib/redis";
 export const POST = async (request: NextRequest) => {
   try {
     const { email } = await request.json()
-    const code = Math.floor(Math.random() * 1000000).toString();
+    const code = Math.floor(Math.random() * 1000000).toString()
     await transporter.sendMail({
       from: process.env.SMTP_USER,
       to: email,
       subject: process.env.SMTP_SUBJECT,
       text: process.env.SMTP_TEXT + code,
-    });
-    redis.set(email, code, 'EX', 60 * 5);
+    })
+    redis.set(email, code, 'EX', 60 * 5)
 
     return NextResponse.json(ResponseOK(null))
   } catch (error) {
--- fullstack/src/lib/nodemailer.ts
+++ fullstack/src/lib/nodemailer.ts
@@ -7,9 +7,9 @@ const transporter = nodemailer.createTransport({
   requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    pass: process.env.SMTP_PASS
   },
   logger: false
-});
+})
 
 export default transporter;
