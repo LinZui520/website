b06f1346c586af4b1ff70dfd6e09ab5db5a591a4
Yangming He <yangminghe20@gmail.com>
2024-05-17 14:06:35 +0800

    style: auth

--- nextjs/src/app/(web)/(auth)/login/page.tsx
+++ nextjs/src/app/(web)/(auth)/login/page.tsx
@@ -71,39 +71,51 @@ const Page = () => {
           >
             
             <Input
-              type="email"
-              color="default"
-              label="邮箱"
-              value={email}
+              type="email" color="default"
+              label="邮箱" value={email}
               onChange={(e) => setEmail(e.target.value)}
+              endContent={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+                  width="18" height="18"
+                >
+                  <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/>
+                </svg>
+              }
             />
-            
+
           </motion.div>
 
           <motion.div
             className="h-[50px] w-full"
-            initial={{ opacity: 0, x: -200 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.618, type: "spring", stiffness: 100, damping: 10, delay: 0.6 }}
+            initial={{opacity: 0, x: -200}}
+            animate={{opacity: 1, x: 0}}
+            transition={{duration: 0.618, type: "spring", stiffness: 100, damping: 10, delay: 0.6}}
           >
 
             <Input
-              type="password"
-              color="default"
-              label="密码"
-              value={password}
+              type="password" color="default"
+              label="密码" value={password}
               onChange={(e) => setPassword(e.target.value)}
+              endContent={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+                  width="18" height="18"
+                >
+                  <path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/>
+                </svg>
+              }
             />
           </motion.div>
 
           <motion.div
             className=" w-full flex flex-row justify-between items-center"
-            initial={{ opacity: 0, x: -200 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.618, type: "spring", stiffness: 100, damping: 10, delay: 0.8 }}
+            initial={{opacity: 0, x: -200}}
+            animate={{opacity: 1, x: 0}}
+            transition={{duration: 0.618, type: "spring", stiffness: 100, damping: 10, delay: 0.8}}
           >
-            <motion.div 
-              initial={{ scale: 0.8 }}
+            <motion.div
+              initial={{scale: 0.8}}
               whileHover={{ scale: 1, opacity: 0.9 }}
               whileTap={{ scale: 0.8 }}
               onClick={() => router.push('/security')}
