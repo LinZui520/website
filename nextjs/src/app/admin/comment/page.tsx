b59d57243ebfc7092b867d6014ee9bb2dbd8cfbf
Yangming He <yangminghe20@gmail.com>
2024-05-21 11:25:55 +0800

    style: 维护中

--- nextjs/src/app/admin/comment/page.tsx
+++ nextjs/src/app/admin/comment/page.tsx
@@ -2,9 +2,8 @@
 
 const Page = async () => {
   return (
-
-    <div>
-      <h1>Comment</h1>
+    <div className={"h-calc-84 w-full bg-[#fbfbfd] flex justify-center items-center"}>
+      <span className={"sm:text-[18px] md:text-[22px] lg:text-[27px]"}>维护中</span>
     </div>
   );
 }
