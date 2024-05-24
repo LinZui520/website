95a44909a085b34da157fe23dc52710f0b3f959b
Yangming He <yangminghe20@gmail.com>
2024-05-24 20:01:03 +0800

    style: good home page

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -13,9 +13,18 @@ const Home = () => {
 
   return (
     <>
-      <div className={"bg-[#fbfbfd] relative z-10 flex flex-col items-center"}>
-        <div className={"h-screen sm:w-[96vw] w-[80vw] flex flex-col justify-center"}>
-          <div className={"text-[#1d1d1f] flex sm:text-[36px] md:text-[54px] lg:text-[64px] mt-[32px] self-start"}>
+      <div className={"w-full bg-[#fbfbfd] relative z-10 flex flex-col items-center"}>
+        <div className={"h-screen relative select-none sm:w-[96vw] w-[80vw] flex flex-col justify-center"}>
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+            className={"absolute top-[100vh] sm:w-[36px] md:w-[54px] lg:w-[64px] sm:h-[54px] md:h-[81px] lg:h-[96px]"}
+            initial={{ y: -128 }}
+          >
+            <path fill="#1d1d1f" d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/>
+          </motion.svg>
+
+          <div
+            className={"text-[#1d1d1f] flex justify-center items-center sm:text-[36px] md:text-[54px] lg:text-[64px] mt-[32px] self-start"}>
             <motion.div
               initial={{ scaleY: -1, y: 64 }}
               whileInView={{ scaleY: 1, y: 0 }}
@@ -49,6 +58,7 @@ const Home = () => {
                 stiffness: 65, damping: 20,
                 repeat: Infinity, repeatType: "reverse"
               }}
+              className={"text-[#11efef] sm:text-[54px] md:text-[64px] lg:text-[78px]"}
             >贺
             </motion.div>
 
@@ -59,6 +69,7 @@ const Home = () => {
                 delay: 1, type: "spring",
                 stiffness: 260, damping: 20
               }}
+              className={"text-[#11efef] sm:text-[54px] md:text-[64px] lg:text-[78px]"}
             >{"\u00A0"}阳
             </motion.div>
 
@@ -69,11 +80,37 @@ const Home = () => {
                 delay: 1, type: "spring",
                 stiffness: 260, damping: 20
               }}
+              className={"text-[#11efef] sm:text-[54px] md:text-[64px] lg:text-[78px]"}
             >明
             </motion.div>
+
+            {"\u00A0"}{"\u00A0"}{"\u00A0"}{"\u00A0"}
+
+            <motion.svg
+              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+              className={"sm:w-[36px] md:w-[54px] lg:w-[64px] sm:h-[54px] md:h-[81px] lg:h-[96px]"}
+              initial={{ scale: 0, opacity: 0 }}
+              whileInView={{
+                scale: 1, opacity: 1,
+                transition: {
+                  delay: 1.5, type: "spring",
+                  stiffness: 260, damping: 20
+                }
+              }}
+              viewport={{ once: true }}
+              animate={{ rotate: [0, 360] }}
+              transition={{
+                delay: 1.5, duration: 1,
+                type: "spring", damping: 64,
+                repeat: Infinity
+              }}
+            >
+              <path fill="#1d1d1f" d="M258.6 0c-1.7 0-3.4 .1-5.1 .5C168 17 115.6 102.3 130.5 189.3c2.9 17 8.4 32.9 15.9 47.4L32 224H29.4C13.2 224 0 237.2 0 253.4c0 1.7 .1 3.4 .5 5.1C17 344 102.3 396.4 189.3 381.5c17-2.9 32.9-8.4 47.4-15.9L224 480v2.6c0 16.2 13.2 29.4 29.4 29.4c1.7 0 3.4-.1 5.1-.5C344 495 396.4 409.7 381.5 322.7c-2.9-17-8.4-32.9-15.9-47.4L480 288h2.6c16.2 0 29.4-13.2 29.4-29.4c0-1.7-.1-3.4-.5-5.1C495 168 409.7 115.6 322.7 130.5c-17 2.9-32.9 8.4-47.4 15.9L288 32V29.4C288 13.2 274.8 0 258.6 0zM256 224a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/>
+            </motion.svg>
           </div>
 
-          <div className={"text-[#1d1d1f] flex sm:text-[36px] md:text-[54px] lg:text-[64px] mt-[32px] self-end"}>
+          <div
+            className={"text-[#1d1d1f] flex justify-center items-center sm:text-[36px] md:text-[54px] lg:text-[64px] mt-[32px] self-end"}>
             <motion.div
               initial={{ scale: 0 }}
               whileInView={{ scale: 1, transition: { delay: 1.5 } }}
@@ -97,7 +134,7 @@ const Home = () => {
                 type: "spring",
                 stiffness: 65, damping: 20,
               }}
-            >个
+            >个{"\u00A0"}
             </motion.div>
 
             <motion.svg
@@ -128,7 +165,8 @@ const Home = () => {
                 type: "spring",
                 stiffness: 65, damping: 20,
               }}
-            >编程
+              className={"text-[#11efef] sm:text-[54px] md:text-[64px] lg:text-[78px]"}
+            >{"\u00A0"}编程
             </motion.div>
 
             <motion.div
@@ -139,7 +177,7 @@ const Home = () => {
                 delay: 1.5, type: "spring",
                 stiffness: 65, damping: 20
               }}
-            >の
+            >{"\u00A0"}の{"\u00A0"}
             </motion.div>
 
             <motion.div
@@ -155,6 +193,8 @@ const Home = () => {
             </motion.div>
           </div>
         </div>
+
+
       </div>
       <Footer/>
     </>
