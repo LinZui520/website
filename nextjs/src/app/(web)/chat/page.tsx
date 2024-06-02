5a7120996102e0030de09f01c76108ecf45d6472
Yangming He <yangminghe20@gmail.com>
2024-06-02 18:22:49 +0800

    style: chat and photo

--- nextjs/src/app/(web)/chat/page.tsx
+++ nextjs/src/app/(web)/chat/page.tsx
@@ -2,7 +2,7 @@
 const Page = async () => {
 
   return (
-    <div className={"h-calc-84 w-full bg-[#fbfbfd] flex justify-center items-center"}>
+    <div className={"h-screen w-full bg-[#fbfbfd] flex justify-center items-center"}>
       <span className={"sm:text-[18px] md:text-[22px] lg:text-[27px]"}>维护中</span>
     </div>
   );
