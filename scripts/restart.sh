c8dd51fcac5bf8d10e34e85e8e803407fa99db10
Yangming He <yangminghe20@gmail.com>
2024-06-10 18:18:29 +0800

    fix: 修复重新部署脚本bug

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -11,13 +11,14 @@ sed -i "s/AUTH_SECRET=${AUTH_SECRET}/AUTH_SECRET=\[FILTERED\]/g" /root/website/n
 sed -i "s/GITHUB_WEBHOOK_SECRET=${GITHUB_WEBHOOK_SECRET}/GITHUB_WEBHOOK_SECRET=\[FILTERED\]/g" /root/website/nextjs/.env
 
 git pull origin main
+GIT_PULL_RESULT=$?
 
 sed -i "s/SMTP_PASS=\[FILTERED\]/SMTP_PASS=${SMTP_PASS}/g" /root/website/nextjs/.env
 sed -i "s/AUTH_SECRET=\[FILTERED\]/AUTH_SECRET=${AUTH_SECRET}/g" /root/website/nextjs/.env
 sed -i "s/GITHUB_WEBHOOK_SECRET=\[FILTERED\]/GITHUB_WEBHOOK_SECRET=${GITHUB_WEBHOOK_SECRET}/g" /root/website/nextjs/.env
 
 
-if [ ! $? -eq 0 ]; then
+if [ ! $GIT_PULL_RESULT -eq 0 ]; then
 	exit 1
 fi
 
