92d218fb89c08163c03148c162adaae07dfeefae
Yangming He <yangminghe20@gmail.com>
2024-06-13 20:50:54 +0800

    refactor: database

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,7 +3,7 @@
 rm -rf /root/data/*
 mkdir -p /root/data
 
-cp -r /root/website/mysql/data/image /root/data/
+cp -r /root/website/database/image /root/data/
 
-/usr/bin/docker exec $(docker ps -a --format '{{.Names}}' | grep -E "^website[-_]mysql[-_]1$") bash -c "mysqldump -uroot -p123456 website > /tmp/backup.sql"
-/usr/bin/docker cp $(docker ps -a --format '{{.Names}}' | grep -E "^website[-_]mysql[-_]1$"):/tmp/backup.sql /root/data/backup.sql
+/usr/bin/docker exec $(docker ps -a --format '{{.Names}}' | grep -E "^website[-_]database[-_]1$") bash -c "mysqldump -uroot -p123456 website > /tmp/backup.sql"
+/usr/bin/docker cp $(docker ps -a --format '{{.Names}}' | grep -E "^website[-_]database[-_]1$"):/tmp/backup.sql /root/data/backup.sql
