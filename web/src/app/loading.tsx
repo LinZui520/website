a775cb70aeee3b9275a9e2b655f819f813e6ed32
Yangming He <yangminghe20@gmail.com>
2024-06-15 15:16:50 +0800

    feat: 更改字体

--- web/src/app/loading.tsx
+++ web/src/app/loading.tsx
@@ -3,7 +3,7 @@
 const Loading = () => {
   return (
     <div className={"h-screen max-h-full w-full bg-[#fbfbfd] flex justify-center items-center"}>
-      <span className={"font-mono text-[#1d1d1f] text-[27px]"}>
+      <span className={"text-[#1d1d1f] text-[27px]"}>
         Loading...
       </span>
     </div>
--- web/src/app/not-found.tsx
+++ web/src/app/not-found.tsx
@@ -3,7 +3,7 @@
 const NotFound = async () => {
   return (
     <div className={"h-screen max-h-full w-full bg-[#fbfbfd] flex justify-center items-center"}>
-      <span className={"font-mono text-[#1d1d1f] text-[27px]"}>
+      <span className={"text-[#1d1d1f] text-[27px]"}>
         404 - Not Found
       </span>
     </div>
