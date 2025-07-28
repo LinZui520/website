eccaac2a216142458e54005837b977836a1cfb72
Yangming He <yangminghe20@gmail.com>
2025-07-28 23:24:24 +0800

    feat: 优化样式 (#221)

--- web/src/components/Logo.tsx
+++ web/src/components/Logo.tsx
@@ -1,15 +1,120 @@
+// 灵感来源 https://www.makemepulse.com/
+import { useEffect, useRef, useState } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
 type Props = {
   className?: string;
 };
 
 const Logo = (props: Props) => {
+  const [isHover, setIsHover] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const timeline = useRef<GSAPTimeline | undefined>(undefined);
+  const grayCircleRef = useRef<HTMLDivElement>(null);
+  const blackCircle1Ref = useRef<HTMLDivElement>(null);
+  const blackCircle2Ref = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    const container = containerRef.current;
+    if (!container) return;
+    timeline.current = gsap.timeline();
+
+    timeline.current
+      .to(grayCircleRef.current, {
+        duration: 0.5,
+        x: 30,
+        ease: 'back.out'
+      }, 0)
+      .to(grayCircleRef.current, {
+        duration: 0.5,
+        x: -60,
+        ease: 'back.out'
+      }, 0.5)
+      .to(blackCircle2Ref.current, {
+        duration: 0,
+        opacity: 0,
+        ease: 'none'
+      }, 0.55)
+      .to(blackCircle1Ref.current, {
+        duration: 0,
+        opacity: 0,
+        ease: 'none'
+      }, 0.65)
+      // 灰色球变成黑色球（根据主题变色）
+      .to(grayCircleRef.current, {
+        duration: 0,
+        backgroundColor: window.matchMedia('(prefers-color-scheme: dark)').matches ? '#FAFAFC' : '#1D1D1F',
+        ease: 'none'
+      }, 0.55)
+      .to(grayCircleRef.current, {
+        duration: 0.4,
+        x: 25,
+        ease: 'back.out'
+      }, 1)
+      // 球回归时，文字逐渐显示
+      .to('#char-0', { duration: 0, opacity: 1, ease: 'power2.inOut' }, 1.0)
+      .to('#char-1', { duration: 0, opacity: 1, ease: 'power2.inOut' }, 1.05)
+      .to('#char-2', { duration: 0, opacity: 1, ease: 'power2.inOut' }, 1.1)
+      .to('#char-3', { duration: 0, opacity: 1, ease: 'power2.inOut' }, 1.15)
+      .to('#char-4', { duration: 0, opacity: 1, ease: 'power2.inOut' }, 1.2)
+      .to('#char-5', { duration: 0, opacity: 1, ease: 'power2.inOut' }, 1.25);
+
+  }, { scope: containerRef });
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+    const play = () => setIsHover(true);
+    const reverse = () => setIsHover(false);
+    container.addEventListener('mouseenter', play);
+    container.addEventListener('mouseleave', reverse);
+
+    return () => {
+      container.removeEventListener('mouseenter', play);
+      container.removeEventListener('mouseleave', reverse);
+    };
+  }, []);
+
+  useGSAP(() => isHover ? timeline.current?.play() : timeline.current?.reverse(), { scope: containerRef, dependencies: [isHover] });
 
   return (
-    <div className={`${props.className || ''} flex flex-col items-center justify-center font-serif`}>
-      <span className="text-3xl text-mint-950 dark:text-mint-50">
-        ZhuGui
-      </span>
-      <span className="text-base text-mint-500 line-through">shi hun dan</span>
+    <div
+      className={`${props.className || ''} flex flex-col items-center justify-center font-serif cursor-pointer`}
+      ref={containerRef}
+    >
+      {/* 圆圈动画区域 */}
+      <div className="flex items-center space-x-2 relative">
+        <div
+          className="w-3 h-3 bg-mint-950 dark:bg-mint-50 rounded-full"
+          ref={blackCircle1Ref}
+        >
+        </div>
+        <div
+          className="w-3 h-3 bg-mint-950 dark:bg-mint-50 rounded-full"
+          ref={blackCircle2Ref}
+        >
+        </div>
+        <div
+          className="w-3 h-3 bg-mint-500 rounded-full"
+          ref={grayCircleRef}
+        >
+        </div>
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          {/* 文字 */}
+          <div className="flex">
+            {'ZHUGUI'.split('').map((char, index) => (
+              <span
+                className="text-char text-mint-950 dark:text-mint-50 font-serif tracking-tight opacity-0"
+                id={`char-${index}`}
+                key={index}
+              >
+                {char}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- web/src/pages/index/page.tsx
+++ web/src/pages/index/page.tsx
@@ -7,39 +7,53 @@ const Page = () => {
   const container = useRef<HTMLElement | null>(null);
 
   useGSAP(() => {
-    gsap.from('#he', { y: '-150%', ease: 'back', delay: 0.5, duration: 0.5 });
-    gsap.from('#yang', { x: '150%', ease: 'power2.out', delay: 1, duration: 0.5 });
-    gsap.from('#ming', { x: '150%', ease: 'power2.out', delay: 1.2, duration: 0.5 });
-    [1, 2, 3, 4, 5].map((i) => {
-      gsap.from(`#self-${i}`, { x: '-150%', ease: 'power2.out', delay: 1.5 + i * 0.2, duration: 0.5 });
-    });
-    gsap.from('#life', { x: '150%', ease: 'power2.out', delay: 3, duration: 0.5 });
-    gsap.from('#less', { y: '-150%', ease: 'power2.out', delay: 3.5, duration: 0.5 });
+    gsap.from('#main-title-1', { opacity: 0, y: 50, duration: 1, delay: 1 });
+    gsap.from('#main-title-2', { opacity: 0, y: 50, duration: 1, delay: 1.2 });
+    gsap.from('#main-title-3', { opacity: 0, y: 50, duration: 1, delay: 1.4 });
+    gsap.from('#main-title-4', { opacity: 0, y: 50, duration: 1, delay: 1.6 });
+    gsap.from('#main-title-5', { opacity: 0, y: 50, duration: 1, delay: 1.8 });
+    gsap.from('#main-title-6', { opacity: 0, y: 50, duration: 1, delay: 2.0 });
+    gsap.from('#bottom-quote', { opacity: 0, y: 30, duration: 1, delay: 2.2 });
   }, { scope: container });
 
   return (
     <>
-      <main className="bg-mint-50 dark:bg-mint-950 h-[calc(100vh-4rem)] md:h-[calc(100vh-8rem)] w-screen flex flex-col items-center justify-center overflow-hidden" ref={container}>
-        <div className="flex flex-col md:flex-row justify-between w-1/2 min-w-sm text-mint-950 dark:text-mint-50 border-2 border-mint-950 dark:border-mint-50 p-4">
-          <div className="flex flex-col text-2xl overflow-hidden">
-            <span className="underline text-mint-500" id="self-1">Software Engineer</span>
-            <span className="underline text-mint-500 mt-1" id="self-2">自诩为设计师</span>
-            <span className="underline text-mint-500 mt-1" id="self-3">完美主义者</span>
-            <span className="underline text-mint-500 mt-1" id="self-4">强迫症</span>
-            <span className="underline text-mint-500 mt-1" id="self-5">优雅</span>
-            <div className="mt-4 md:mt-16 overflow-hidden">
-              <span className="text-4xl" id="less" style={{ writingMode: 'vertical-lr' }}>Less is more</span>
-            </div>
+      <main className="bg-mint-50 dark:bg-mint-950 text-mint-950 dark:text-mint-50 min-h-[calc(100vh-4rem)] w-screen flex flex-col items-center pt-32 pb-32" ref={container}>
+
+        {/* Main Content */}
+        <div className="px-6 md:px-8">
+
+          <div className="space-y-4 md:space-y-6">
+            <h2 className="text-6xl md:text-8xl lg:text-9xl font-light tracking-tight" id="main-title-1">
+              我叫贺阳明
+            </h2>
+
+            <h2 className="text-6xl md:text-8xl lg:text-9xl font-light tracking-tight" id="main-title-2">
+              美团软件工程师
+            </h2>
+
+            <h2 className="text-6xl md:text-8xl lg:text-9xl font-light tracking-tight" id="main-title-3">
+              自诩为设计师
+            </h2>
+
+            <h2 className="text-6xl md:text-8xl lg:text-9xl font-light tracking-tight" id="main-title-4">
+              完美主义者
+            </h2>
+
+            <h2 className="text-6xl md:text-8xl lg:text-9xl font-light tracking-tight text-mint-500" id="main-title-5">
+              强迫症患者
+            </h2>
+
+            <h2 className="text-6xl md:text-8xl lg:text-9xl font-light tracking-tight" id="main-title-6">
+              优雅至上
+            </h2>
           </div>
-          <div className="overflow-hidden flex flex-col items-end justify-between">
-            <div className="text-6xl flex flex-col">
-              <span id="he">贺</span>
-              <span id="yang">阳</span>
-              <span id="ming">明</span>
-            </div>
-            <div className="mt-4 md:mt-16" id="life">
-              <span className="text-2xl underline text-mint-500 text-wrap">Life&#39;s not out to get you</span>
-            </div>
+
+          {/* Bottom Quote */}
+          <div className="mt-16 md:mt-24 text-right group">
+            <p className="text-4xl md:text-5xl lg:text-6xl text-mint-500 font-serif italic tracking-tight" id="bottom-quote">
+              Less is more
+            </p>
           </div>
         </div>
       </main>
