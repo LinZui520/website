fd13501d8d03cb978839d027dac72b273fb5ece9
Yangming He <yangminghe20@gmail.com>
2025-08-24 03:41:12 +0800

    fix: 修复图片上传失败bug

--- server/src/routers/photo.rs
+++ server/src/routers/photo.rs
@@ -1,8 +1,8 @@
 use crate::AppState;
 use crate::controllers::photo::{create_photo, delete_photo, read_photo, update_photo};
 use axum::Router;
-use axum::routing::{delete, get, post, put};
 use axum::extract::DefaultBodyLimit;
+use axum::routing::{delete, get, post, put};
 use std::sync::Arc;
 
 pub fn create_photo_router() -> Router<Arc<AppState>> {
--- server/src/routers/picture.rs
+++ server/src/routers/picture.rs
@@ -1,8 +1,8 @@
 use crate::AppState;
 use crate::controllers::picture::{create_picture, delete_picture, read_picture};
 use axum::Router;
-use axum::routing::{delete, get, post};
 use axum::extract::DefaultBodyLimit;
+use axum::routing::{delete, get, post};
 use std::sync::Arc;
 
 pub fn create_picture_router() -> Router<Arc<AppState>> {
